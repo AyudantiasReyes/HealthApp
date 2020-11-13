6e924e2b8fb60aeac1b630458a3ba4347b604721
Diego <diego.caudillo@uabc.edu.mx>
2020-11-13 10:53:17 -0800

    UI Register

---
+++ vision/final/app/src/main/java/com/google/firebase/codelab/UI/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.google.firebase.codelab.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.codelab.labelScannerUABC.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
