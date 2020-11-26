7cda64c80017e064bd213a136349bf4568a62fce
Fubbledisrael <israel97_@hotmail.com>
2020-11-25 22:34:11 -0800

    connect DB

--- vision/final/app/src/main/java/com/google/firebase/codelab/UI/LoginActivity.java
+++ vision/final/app/src/main/java/com/google/firebase/codelab/UI/LoginActivity.java
@@ -1,23 +1,37 @@
 package com.google.firebase.codelab.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
-
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.firebase.codelab.labelScannerUABC.MainMenuActivity;
 import com.google.firebase.codelab.labelScannerUABC.R;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
-    Button login_button;
-    Button register_button;
+    EditText edt_email, edt_pass;
+    Button login_button, register_button;
+    String email, pass;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Get the EditTexts from the Layout
+        edt_email = findViewById(R.id.et_email);
+        edt_pass = findViewById(R.id.et_password);
 
         //Get the BUTTONS from the Layout
         login_button = findViewById(R.id.button_login);
@@ -26,12 +40,13 @@ public class LoginActivity extends AppCompatActivity {
         login_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                /*
-                * Aqui va el codigo necesario para validar que los
-                * datos ingresados en el Login sean correctos
-                */
-                Toast.makeText(LoginActivity.this, "Sesión Iniciada",Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(getApplicationContext(), MainMenuActivity.class));
+                email = edt_email.getText().toString();
+                pass = edt_pass.getText().toString();
+                //Verificar campos vacios
+                if(email.isEmpty() || pass.isEmpty())
+                    Toast.makeText(LoginActivity.this,"Llenar todos los campos",Toast.LENGTH_SHORT).show();
+                else
+                    ValidateUser();
             }
         });
 
@@ -41,5 +56,33 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
             }
         });
+    }
+
+    private void ValidateUser(){
+        String URL = "http://conisoft.org/HealthApp/validateUser.php";
+        StringRequest request = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if(!response.isEmpty())                                                         //response contiene json de la consulta en bd
+                    startActivity(new Intent(getApplicationContext(), MainMenuActivity.class));
+                else
+                    Toast.makeText(LoginActivity.this,"Correo o contraseña invalido",Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(LoginActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {             //parametros que se envian con metodo POST
+                Map<String,String> parametros = new HashMap<String,String>();
+                parametros.put("email",edt_email.getText().toString());
+                parametros.put("pass",edt_pass.getText().toString());
+                return parametros;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(request);
     }
 }
--- vision/final/app/src/main/java/com/google/firebase/codelab/UI/RegisterActivity.java
+++ vision/final/app/src/main/java/com/google/firebase/codelab/UI/RegisterActivity.java
@@ -1,16 +1,86 @@
 package com.google.firebase.codelab.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.firebase.codelab.labelScannerUABC.R;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
+    EditText edtName, edtLastName, edtEmail, edtPass;
+    Button btnRegister;
+    String name, lastname, email, pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //Get the EditTexts from the Layout
+        edtName = findViewById(R.id.editTextTextPersonName);
+        edtLastName = findViewById(R.id.editTextTextPersonName2);
+        edtEmail = findViewById(R.id.editTextTextEmailAddress);
+        edtPass = findViewById(R.id.editTextTextPassword);
+
+        //Get the BUTTONS from the Layout
+        btnRegister = findViewById(R.id.button);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                name = edtName.getText().toString();
+                lastname = edtLastName.getText().toString();
+                email = edtEmail.getText().toString();
+                pass = edtPass.getText().toString();
+                if(name.isEmpty() || lastname.isEmpty() || email.isEmpty() || pass.isEmpty())
+                    Toast.makeText(RegisterActivity.this,"Llenar todos los campos",Toast.LENGTH_SHORT).show();
+                else
+                    RegisterUser();
+            }
+        });
+    }
+
+    private void RegisterUser(){
+        String URL = "http://conisoft.org/HealthApp/registerUser.php";
+        StringRequest request = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if(response.equals("true")){
+                    Toast.makeText(RegisterActivity.this,"Usuario registrado",Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                }
+                else
+                    Toast.makeText(RegisterActivity.this,"Error, correo ya registrado",Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(RegisterActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> parametros = new HashMap<String,String>();
+                parametros.put("name",edtName.getText().toString());
+                parametros.put("lastname",edtLastName.getText().toString());
+                parametros.put("email",edtEmail.getText().toString());
+                parametros.put("pass",edtPass.getText().toString());
+                return parametros;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(request);
     }
 }
